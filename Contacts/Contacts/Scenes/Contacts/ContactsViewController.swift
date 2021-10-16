5d5b71c9684c6c927629f48c2595568334677a87
Anudeep Gone <anudeepgone@gmail.com>
2021-10-16 22:11:11 +0800

    setup project and folder structure

---
+++ Contacts/Contacts/Scenes/Contacts/ContactsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ContactsViewController.swift
+//  Contacts
+//
+//  Created by Anudeep Gone on 16/10/21.
+//
+
+import UIKit
+
+class ContactsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
